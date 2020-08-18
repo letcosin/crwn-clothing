5aab122ab62b9bd8f3f30b053f332e75e0dc613a
Leticia Cosin <leticia.cosin@hotmail.com>
2020-08-18 14:45:45 -0300

    alteraçoes layout

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -6,31 +6,31 @@ const HomePage = () => (
       <div className="menu-item">
         <div className="content">
           <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
+          <span className="subtitle">SHOP NOW!</span>
         </div>
       </div>
       <div className="menu-item">
         <div className="content">
           <h1 className="title">JACKETS</h1>
-          <span className="subtitle">SHOP NOW</span>
+          <span className="subtitle">SHOP NOW!</span>
         </div>
       </div>
       <div className="menu-item">
         <div className="content">
           <h1 className="title">SNEAKERS</h1>
-          <span className="subtitle">SHOP NOW</span>
+          <span className="subtitle">SHOP NOW!</span>
         </div>
       </div>
       <div className="menu-item">
         <div className="content">
           <h1 className="title">WOMENS</h1>
-          <span className="subtitle">SHOP NOW</span>
+          <span className="subtitle">SHOP NOW!</span>
         </div>
       </div>
       <div className="menu-item">
         <div className="content">
           <h1 className="title">MENS</h1>
-          <span className="subtitle">SHOP NOW</span>
+          <span className="subtitle">SHOP NOW!</span>
         </div>
       </div>
     </div>
